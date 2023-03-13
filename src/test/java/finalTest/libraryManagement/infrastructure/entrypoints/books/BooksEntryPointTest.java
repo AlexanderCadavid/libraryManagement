ad785203fb72ebe958cf8b1545b89bbe34aba537
ALEXANDER CADAVID <alexander.cadavid@ias.com.co>
2023-03-13 16:56:59 -0500

    Creacion estructura pruebas unitarias

--- src/test/java/finalTest/libraryManagement/infrastructure/entrypoints/books/BooksEntryPointTest.java
+++ src/test/java/finalTest/libraryManagement/infrastructure/entrypoints/books/BooksEntryPointTest.java
@@ -1,8 +1,19 @@
 package finalTest.libraryManagement.infrastructure.entrypoints.books;
 
+import finalTest.libraryManagement.domain.UseCases.books.BooksUseCase;
 import finalTest.libraryManagement.domain.models.books.Books;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(Books.class)
 public class BooksEntryPointTest {
+
+    @MockBean
+    private BooksUseCase booksUseCase;
+
+    @Autowired
+    private MockMvc mockMvc;
+
 }
--- src/test/java/finalTest/libraryManagement/infrastructure/entrypoints/users/UsersEntryPointTest.java
+++ src/test/java/finalTest/libraryManagement/infrastructure/entrypoints/users/UsersEntryPointTest.java
@@ -1,8 +1,18 @@
 package finalTest.libraryManagement.infrastructure.entrypoints.users;
 
+import finalTest.libraryManagement.domain.UseCases.users.UsersUseCase;
 import finalTest.libraryManagement.domain.models.users.Users;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(Users.class)
 public class UsersEntryPointTest {
+
+    @MockBean
+    private UsersUseCase usersUseCase;
+
+    @Autowired
+    private MockMvc mockMvc;
 }
